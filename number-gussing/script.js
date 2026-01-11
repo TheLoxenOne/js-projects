d37f77145600392c8ad53ff13488d0e059beaa30
TheLoxenOne <theloxenone@gmail.com>
2026-01-11 07:42:14 +0300

    added jsvascript and update ui/ux

---
+++ number-gussing/script.js
@@ -0,0 +1,5 @@
+const numberInput = document.querySelector("input");
+const randomNumber = Math.floor(Math.random() * 100 + 1);
+// if (Number(numberInput.value) === randomNumber) {
+// }
+console.log(randomNumber)
