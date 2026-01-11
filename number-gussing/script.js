576dbcc09ac68a16072c0d6408b8a53b08719681
TheLoxenOne <theloxenone@gmail.com>
2026-01-11 14:33:26 +0300

    improved hearts added some js

--- number-gussing/script.js
+++ number-gussing/script.js
@@ -1,5 +1,11 @@
-const numberInput = document.querySelector("input");
-const randomNumber = Math.floor(Math.random() * 100 + 1);
-// if (Number(numberInput.value) === randomNumber) {
-// }
-console.log(randomNumber)
+const numberInput = document.querySelector(".input");
+const infoCard = document.querySelector(".infoCard")
+const randomNumber = 63;
+const submit = document.querySelector(".submit")
+submit.onclick = checkNumber
+function checkNumber() {
+  if (Number(numberInput.value) === randomNumber) {
+infoCard.style.marginTop = "0vh"
+}
+}
+
