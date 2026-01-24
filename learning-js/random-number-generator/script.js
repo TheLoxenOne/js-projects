989b99c9bd0467147516535aa3c1174b7f562886
TheLoxenOne <theloxenone@gmail.com>
2026-01-24 07:24:58 +0300

    finished creating random number generator

--- learning-js/random-number-generator/script.js
+++ learning-js/random-number-generator/script.js
@@ -1,6 +1,19 @@
-const pressParticle = document.getElementById("pressParticle")
-const generateBtn = document.querySelector(".generateBtn")
-generateBtn.onclick = () => {
-  pressParticle.stop()
-  pressParticle.play()
+const pressParticle = document.getElementById("pressParticle");
+const generateBtn = document.querySelector(".generateBtn");
+const min = document.querySelector(".toNumberContainer");
+const max = document.querySelector(".fromNumberContainer");
+
+if (generateBtn) {
+  generateBtn.onclick = () => {
+    pressParticle.stop();
+    pressParticle.play();
+    const randomNum =
+      Math.floor(Math.random() * (Number(max.value) - Number(min.value))) +
+      Number(min.value);
+    document.querySelector(".number").textContent = randomNum;
+    document.querySelector(".number").style.display = "block";
+    
+  };
 }
+
+console.log(randomNum);
